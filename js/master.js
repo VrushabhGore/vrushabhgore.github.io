5e51dc2d544d530f13f2f5081c4de07a4214c8f9
Vrushabh Gore <vrushabhgore4@gmail.com>
2019-08-10 21:29:52 -0700

    added Education Section

--- js/master.js
+++ js/master.js
@@ -4,4 +4,17 @@ jQuery(document).ready(function(){
 			width:jQuery(this).attr('data-percent')
 		},6000);
 	});
+	var swiper = new Swiper('.blog-slider', {
+		spaceBetween: 30,
+		effect: 'fade',
+		loop: true,
+		mousewheel: {
+		  invert: false,
+		},
+		// autoHeight: true,
+		pagination: {
+		  el: '.blog-slider__pagination',
+		  clickable: true,
+		}
+	  });
 });
